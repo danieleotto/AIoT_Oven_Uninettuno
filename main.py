bfc0b2d0b9084bc6945f25905bc6038c8bc418ce
Daniele Ottorogo <143402662+danieleotto@users.noreply.github.com>
2026-04-22 14:51:12 +0200

    Added main temperature reader

---
+++ main.py
@@ -0,0 +1,26 @@
+import sys, time
+from max6675lib import MAX6675
+
+P_SCK = 8
+P_CS = 7
+P_DO = 5
+
+tc = MAX6675(P_SCK, P_CS, P_DO)
+
+try:
+    print("Press CTRL+C to exit")
+    start_time = time.time()
+    while True:
+        temp = MAX6675.readTempC()
+        elapsed_time = time.time() - start_time
+        if temp is None:
+            sys.stdout.write(f"\rTermocoppia non collegata.")
+        else:
+            sys.stdout.write(f"\rTemperatura: {temp:.2f} °C | Tempo: {elapsed_time:.2f} s")
+        sys.stdout.flush()
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    print("Exit")
+
+
