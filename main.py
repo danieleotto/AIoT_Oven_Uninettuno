57a6c9c73eb8629ecfee7f73429abaffa06cc028
Daniele Ottorogo <143402662+danieleotto@users.noreply.github.com>
2026-04-22 15:03:55 +0200

    added log post exit

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ P_DO = 5
 
 tc = MAX6675(P_SCK, P_CS, P_DO)
 
+x_time = []
+y_temp = []
+
 try:
     print("Press CTRL+C to exit")
     start_time = time.time()
@@ -16,11 +19,15 @@ try:
         if temp is None:
             sys.stdout.write(f"\rTermocoppia non collegata.")
         else:
+            x_time.append(elapsed_time)
+            y_temp.append(temp)
             sys.stdout.write(f"\rTemperatura: {temp:.2f} °C | Tempo: {elapsed_time:.2f} s")
         sys.stdout.flush()
         time.sleep(0.5)
 
 except KeyboardInterrupt:
-    print("Exit")
+    print("\nExit")
+    for values in y_temp:
+        print(f"Temperatura: {y_temp[values]:.2f} °C | Tempo: {x_time[values]:.2f} s")
 
 
