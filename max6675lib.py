9743e8b4bfb8d9be4022608e7d9038bbc86566bf
Daniele Ottorogo <143402662+danieleotto@users.noreply.github.com>
2026-04-22 14:40:48 +0200

    PyCharm Changes

--- max6675lib.py
+++ max6675lib.py
@@ -2,46 +2,45 @@ import wiringpi as wp
 from wiringpi import GPIO
 import time
 
-class Thermocouple6675(object):
-    def __init__(self, _sck, _cs, _do):
-        self.sck = _sck
-        self.cs = _cs
-        self.do = _do
+class MAX6675(object):
+    def __init__(self, PIN_SCK, PIN_CS, PIN_DO):
+        self.PIN_SCK = PIN_SCK
+        self.PIN_CS = PIN_CS
+        self.PIN_DO = PIN_DO
 
         wp.wiringPiSetup()
 
-        wp.pinMode(self.sck, wp.OUTPUT)
-        wp.pinMode(self.cs, wp.OUTPUT)
-        wp.pinMode(self.do, wp.INPUT)
+        wp.pinMode(self.PIN_SCK, GPIO.OUTPUT)
+        wp.pinMode(self.PIN_CS, GPIO.OUTPUT)
+        wp.pinMode(self.PIN_DO, GPIO.INPUT)
 
-        wp.digitalWrite(self.cs, 1)
-        wp.digitalWrite(self.sck, 0)
+        wp.digitalWrite(self.PIN_SCK, 0)
+        wp.digitalWrite(self.PIN_CS, 1)
 
 
     def readTempC(self):
-        wp.digitalWrite(self.cs, 0)
+        wp.digitalWrite(self.PIN_CS, 0)
         time.sleep(0.001)
 
         value = 0
 
-        for i in range (16):
-            wp.digitalWrite(self.sck, 1)
+        for i in range(16):
+            wp.digitalWrite(self.PIN_SCK, 1)
             time.sleep(0.001)
-
             value <<= 1
-            if wp.digitalRead(self.do):
+            if wp.digitalRead(self.PIN_DO, 0):
                 value |= 1
-
-            wp.digitalWrite(self.sck, 0)
+            wp.digitalWrite(self.PIN_SCK, 0)
             time.sleep(0.001)
 
-        wp.digitalWrite(self.cs, 1)
+        wp.digitalWrite(self.PIN_CS, 1)
 
         if value & 0x04:
             return None
 
         temp_c = (value >> 3) * 0.25
         return temp_c
+
 
     def readTempF(self):
         temp_f = (self.readTempC() * (9/5)) + 32
