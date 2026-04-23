1be215a7953b24f7449460b808383effbb2f3ede
Daniele Ottorogo <daniele.ottorogo@gmail.com>
2026-04-23 07:40:40 +0200

    fixed incorrect syntax

--- sqlite3db.py
+++ sqlite3db.py
@@ -7,6 +7,6 @@ class SQLite3DB:
         self.cursor.execute("CREATE TABLE IF NOT EXISTS stato (id INTEGER, timestamp TEXT, elapsedTime REAL, temperatura REAL, ssr1state BOOLEAN, ssr2state BOOLEAN)")
 
     def add(self, idstate, timestamp, elapsedtime, temperatura, ssr1state, ssr2state):
-        self.cursor.execute("INSERT INTO stato (?, ?, ?, ?, ? ?)", (idstate, timestamp, elapsedtime, temperatura, ssr1state, ssr2state))
+        self.cursor.execute("INSERT INTO stato VALUES (?, ?, ?, ?, ?, ?)", (idstate, timestamp, elapsedtime, temperatura, ssr1state, ssr2state))
         self.conn.commit()
 
