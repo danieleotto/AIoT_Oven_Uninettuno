2dfcf383f9f2851c1de730b9dfd379bf165c2b34
Daniele Ottorogo <143402662+danieleotto@users.noreply.github.com>
2026-05-05 16:29:50 +0200

    fix

--- debug_test_sensors.py
+++ debug_test_sensors.py
@@ -15,24 +15,25 @@ tc = MAX6675(TC_SCK,TC_CS,TC_DO, sample_size)
 startTime = lastTime = time.time()
 temp = t = 0
 try:
+    tc.buffer.clear()
+    print(f"Buffer: {tc.buffer}  |  LastTemp: {t}  | LastAVG: {temp}")
+    time.sleep(1)
+    print(f"Inizializzazione sonda.\nEseguo {tc.sample_size} letture con intervallo 1 s.\n")
+    while len(tc.buffer) != tc.sample_size:
+        for i in range(1,tc.sample_size + 1):
+            t = tc.readTempC_average()
+            time.sleep(0.3)
+            sys.stdout.write("\033[F\033[K")
+            sys.stdout.write("\033[F\033[K")
+            text = "* " * i + "  " * (tc.sample_size - i)
+            print(f"{text} {i}/{tc.sample_size}")
+            print(f"Buffer: {tc.buffer}  |  LastTemp: {t}  | LastAVG: {temp}")
+    print("\n")
+
     while True:
-        tc.buffer.clear()
-        print(f"Buffer: {tc.buffer}  |  LastTemp: {t}  | LastAVG: {temp}")
-        time.sleep(1)
-        print(f"Inizializzazione sonda.\nEseguo {tc.sample_size} letture con intervallo 1 s.\n")
-        while len(tc.buffer) != tc.sample_size:
-            for i in range(1,tc.sample_size + 1):
-                t = tc.readTempC_average()
-                time.sleep(1)
-                sys.stdout.write("\033[F\033[K")
-                sys.stdout.write("\033[F\033[K")
-                text = "* " * i + "  " * (tc.sample_size - i)
-                print(f"{text} {i}/{tc.sample_size}")
-                print(f"Buffer: {tc.buffer}  |  LastTemp: {t}  | LastAVG: {temp}")
-        print("\n")
         print(f"Buffer: {tc.buffer}  |  LastTemp: {t}  | LastAVG: {temp}")
         t = tc.readTC()
         temp = tc.readTempC_average()
-        time.sleep(1)
+        time.sleep(0.5)
 except KeyboardInterrupt:
     input("Terminato")
