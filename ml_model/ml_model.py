13791480b7cb2f0f627d4d8d33caa785883d5f1e
Daniele Ottorogo <143402662+danieleotto@users.noreply.github.com>
2026-06-13 00:24:10 +0200

    debug previsione temperatura

--- ml_model/ml_model.py
+++ ml_model/ml_model.py
@@ -53,25 +53,22 @@ class MLModel:
 
         match step:
             case "Riscaldamento":
-                if temp_forno < temp_target:
-                    if temp_target - 10 < temp_futura_prevista <= temp_target -5:
-                        output_corretto = pid_output * 0.7
-                    elif temp_target - 5 < temp_futura_prevista <= temp_target:
-                        output_corretto = pid_output * 0.5
-                    elif temp_futura_prevista > temp_target:
-                        output_corretto = pid_output * 0.3
-                    else:
-                        output_corretto = pid_output
+                if temp_target - 15 < temp_futura_prevista <= temp_target - 5:
+                    output_corretto = pid_output * 0.7
+                elif temp_target - 5 < temp_futura_prevista <= temp_target:
+                    output_corretto = pid_output * 0.5
+                elif temp_futura_prevista > temp_target:
+                    output_corretto = pid_output * 0.3
                 else:
                     output_corretto = pid_output
             case "Essicatura":
                 if temp_forno > temp_target:
                     if temp_target - 5 < temp_futura_prevista < temp_target:
-                        output_corretto = pid_output + 0.2
+                        output_corretto = pid_output + 0.1
                     elif temp_target - 10 < temp_futura_prevista <= 5:
-                        output_corretto = pid_output + 0.35
+                        output_corretto = pid_output + 0.2
                     elif temp_futura_prevista <= temp_target - 10:
-                        output_corretto = pid_output + 0.6
+                        output_corretto = pid_output + 0.3
                     else:
                         output_corretto = pid_output
                 elif temp_forno < temp_target:
@@ -88,7 +85,5 @@ class MLModel:
             case _:
                 output_corretto = pid_output
 
-
         output_corretto = max(0, min(1, output_corretto))
-
         return output_corretto
