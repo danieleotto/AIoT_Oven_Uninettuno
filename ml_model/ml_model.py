92ccb43b4cc0f540a9502405bbc3fa180a0adcb5
Daniele Ottorogo <143402662+danieleotto@users.noreply.github.com>
2026-06-15 08:13:03 +0200

    fix output machine learnign

--- ml_model/ml_model.py
+++ ml_model/ml_model.py
@@ -53,12 +53,14 @@ class MLModel:
         match step:
             case "Riscaldamento":
                 if temp_forno < temp_target:
-                    if temp_target - 15 < temp_futura_prevista <= temp_target -10:
+                    if temp_target - 25 < temp_futura_prevista <= temp_target - 20:
                         output_corretto = pid_output * 0.6
-                    elif temp_target - 10 < temp_futura_prevista <= temp_target:
+                    elif temp_target - 20 < temp_futura_prevista <= temp_target -15:
                         output_corretto = pid_output * 0.4
+                    elif temp_target - 15 < temp_futura_prevista <= temp_target:
+                        output_corretto = 0.0
                     elif temp_futura_prevista > temp_target:
-                        output_corretto = pid_output * 0.15
+                        output_corretto = 0.0
                     else:
                         output_corretto = pid_output
                 else:
